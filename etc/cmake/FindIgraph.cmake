3f3730ac2eec18be4cdfb747fb7f5a236b4a5ed0
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-07-22 14:29:06 +0200

    delete special stunt and move it to consuming project

--- etc/cmake/FindIgraph.cmake
+++ etc/cmake/FindIgraph.cmake
@@ -12,34 +12,6 @@ if(IGRAPH_PATH})
     message(STATUS "-- IGRAPH_PATH: ${IGRAPH_PATH}")
 endif()
 
-# Special case that is not part of a regular `find_package` call.
-#
-# This code path should be used if the consuming project builds
-# igraph. In this case `libigraph.{so,a}` will not exists when
-# this find-code is run. Therefor the usual `find_path` and
-# `find_libary` commands can never be successfull. That is why
-# an imported target, that points to a non-existing lib and
-# empty include dir is defined here. Make sure to define a
-# dependency for igraphpp to the igraph build target.
-if(IGRAPH_PATH AND IGRAPH_PATH_FORCE)
-    message(STATUS "-- IGRAPH_PATH_FORCE: ${IGRAPH_PATH_FORCE}")
-
-    set(libname libigraph.so)
-    if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-        set(libname libigraph.dylib)
-    endif()
-
-    add_library(igraph SHARED IMPORTED)
-    set_property(TARGET igraph PROPERTY IMPORTED_LOCATION ${IGRAPH_PATH}/lib/${libname})
-    #This directory must exist to pass it to `target_include_directories`
-    file(MAKE_DIRECTORY ${IGRAPH_PATH}/include)
-    target_include_directories(igraph INTERFACE ${IGRAPH_PATH}/include)
-
-    set(Igraph_FOUND TRUE PARENT_SCOPE)
-    return()
-endif()
-# Special case - END
-
 if(IGRAPH_PATH)
     set(igraph_no_default_path NO_DEFAULT_PATH)
 else()
