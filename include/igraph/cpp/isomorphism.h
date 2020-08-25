8ee46868969d73f72e8d3c7ce9b05e04d793a1ff
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-08-25 13:41:13 +0200

    Add isomorphism.cpp

--- include/igraph/cpp/isomorphism.h
+++ include/igraph/cpp/isomorphism.h
@@ -3,12 +3,14 @@
 #ifndef IGRAPHPP_ISOMORPHISM_H
 #define IGRAPHPP_ISOMORPHISM_H
 
+#include <igraph/cpp/types.h>
+
 namespace igraph {
 
 class Graph;
 
 /// Checks if two given (di)graphs are isomorphic. A wrapper around isomorphic(...).
-bool isomorphic(const Graph& graph1, const Graph& graph2);
+igraph_bool_t isomorphic(const Graph& graph1, const Graph& graph2);
 
 }         // end of namespace
 
--- src/isomorphism.cpp
+++ src/isomorphism.cpp
@@ -5,9 +5,9 @@
 
 namespace igraph {
 
-    bool isomorphic(const Graph& graph1, const Graph& graph2){
+    igraph_bool_t isomorphic(const Graph& graph1, const Graph& graph2){
         igraph_bool_t result;
-        IGRAPH_TRY(igraph_isomorphic(graph1.c_graph(), graph2.c_graph(), result))
+        IGRAPH_TRY(igraph_isomorphic(graph1.c_graph(), graph2.c_graph(), &result))
         return result;
     }
 
